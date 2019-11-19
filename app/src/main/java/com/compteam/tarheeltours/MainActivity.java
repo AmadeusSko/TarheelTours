709af5dc884beb8ea4dc5befaeff8fe5f555e12b
AmadeusSko <amadeush@live.unc.edu>
2019-11-19 16:45:15 -0500

    Initiated stuff for map permissions and whatnot

--- app/src/main/java/com/compteam/tarheeltours/MainActivity.java
+++ app/src/main/java/com/compteam/tarheeltours/MainActivity.java
@@ -1,14 +1,72 @@
 package com.compteam.tarheeltours;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private static final int REQUEST_CODE = 73;
+    private LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            accessLocation();
+        }
+        else{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            }
+            else{
+                Toast.makeText(this, "Unable to get location permissions.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void accessLocation(){
+        if(!mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+        try {
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000,1,this);
+
+        } catch(SecurityException e){
+            e.printStackTrace();
+            Toast.makeText(this, "Unable to get location updates.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
